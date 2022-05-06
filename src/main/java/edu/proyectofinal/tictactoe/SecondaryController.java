860d10d8271ed5b63eff0d253cf79f044e9c2591
Maria Apa <marialuquelopez@iessoterohernandez.es>
2022-05-06 11:10:50 +0200

    Creando el proyecto

---
+++ src/main/java/edu/proyectofinal/tictactoe/SecondaryController.java
@@ -0,0 +1,12 @@
+package edu.proyectofinal.tictactoe;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
