72b8106fb686d2636bd0c79889ca1d9926956386
Maria Apa <marialuquelopez@iessoterohernandez.es>
2022-06-02 13:05:49 +0200

    arreglos

--- src/main/java/edu/proyectofinal/tictactoe/controller/ComplainController.java
+++ src/main/java/edu/proyectofinal/tictactoe/controller/ComplainController.java
@@ -13,32 +13,57 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+/**
+ * Complain class.
+ *
+ * @author MariaApa
+ * @author Valentina
+ * @author Julia
+ */
 
 
 public class ComplainController implements Initializable {
 
-
+    //UserService object
     private UserService userService;
+
+    //SUggetsionsService object
     private SuggestionsService suggestionsService;
 
+
+    // text field
     @FXML
     private TextField text;
 
+    // text
     @FXML
-    private Button submit;
+    private Text status;
+
+
+    /**
+     * switch to second menu interface.
+     * @param event change interfaces
+     * @throws {@code IOException}
+     */
 
     public void switchToSecondMenu(ActionEvent event) throws IOException {
         App.setStage("secondmenuInterface");
     }
 
 
-
+    /**
+     * Method for initialize suggestion and userservice object
+     * @param url
+     * @param resourceBundle
+     *
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         suggestionsService=new SuggestionsService(new SuggestionsManagerImpl());
@@ -47,7 +72,15 @@ public class ComplainController implements Initializable {
 
     }
 
-    public void submitSuggestions(ActionEvent event) throws IOException {
+
+    /**
+     * Method for send an email with a pdf attach
+     * and update a suggestion on the database
+     * @param event
+     *
+     */
+
+    public void submitSuggestions(ActionEvent event) {
 
         String queja=text.getText();
         String nombre= App.getNamePlayer();
@@ -57,26 +90,31 @@ public class ComplainController implements Initializable {
             int createSuggestion= suggestionsService.insertSuggestion(nombre,queja);
             if(createSuggestion  > 0){
                 // App.setStage("secondmenuInterface");
-                text.setText("");
+                status.setText("");
                 App.setSuggestion(queja);
-                new PdfCreator().createPDF("Suggestions","Thank you for your suggestions. You can see a copy below: ",App.getSuggestion());
-                email(mail);
+                String namePdf= new PdfCreator().createPDF("Suggestions","Thank you for your suggestions. You can see a copy below: ",App.getSuggestion());
+                email(mail,namePdf);
 
 
             }
 
-        } catch (SQLException | ClassNotFoundException | DocumentException | URISyntaxException e) {
+        } catch (SQLException | ClassNotFoundException | DocumentException | URISyntaxException | IOException e) {
             e.printStackTrace();
         }
 
 
     }
 
-    public void email(String to) {
+    /**
+     * Method for send an email
+     * @param to destination
+     * @param pdf pdf file
+     *
+     */
+    public void email(String to, String pdf)  {
 
         try {
-            new Senders().send("tictactoecustomservice@gmail.com", to, "Hey! Check your comment suggestions", "Thanks for your comments! This help us a lot to improve our game! Here you have an" +
-                    "pdf with your suggestions write down");
+            new Senders().send("tictactoecustomservice",to,"Hey! Check your suggestions!","Thanks for your comments! This help us a lot to improve our game! Here you have an pdf with your suggestions write down",pdf);
         } catch (Exception e) {
             System.out.println("Error, email not found");
             e.printStackTrace();
